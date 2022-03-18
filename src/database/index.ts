fbd94d79329ba90f74580a1886940b07f7bff123
tiago luz <tiagoluzgpr@gmail.com>
2022-03-17 22:15:09 -0400

    fix config db

--- src/database/index.ts
+++ src/database/index.ts
@@ -6,7 +6,6 @@ import Address from '../models/Address';
 import Sector from '../models/Sector';
 import Member from '../models/Member';
 import Congregation from '../models/Congregation';
-const dbConfig = require('../teste/database');
 
 dotenv.config();
 
@@ -19,6 +18,17 @@ const models = [
   Congregation
 ]
 
-const dbConnection = new Sequelize({ ...dbConfig, models });
+const dbConnection = new Sequelize({
+  dialect: 'mysql',
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  define: {
+    timestamps: true,
+    underscored: true
+  },
+  models
+});
 
 export default dbConnection;
