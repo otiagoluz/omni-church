92ca8a9b043d75f4164e160e41353c976384d652
tiago luz <tiagoluzgpr@gmail.com>
2022-03-21 22:14:13 -0400

    update insomnia data

--- src/index.ts
+++ src/index.ts
@@ -16,14 +16,14 @@ const app = express();
 app.use(cors({
   origin: 'http://localhost:4200'
 }));
-app.use(express.json());
-app.use(routes);
-
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
 });
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(PORT); 
